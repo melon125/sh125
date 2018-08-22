ff93c642e6129d79ea4305de512163a0790dbecb
1614588384 <1614588384@qq.com>
2018-08-22 11:08:15 +0800

    app

---
+++ Base/get_data.py
@@ -0,0 +1,17 @@
+import yaml, os
+
+class Get_Data:
+
+    def __init__(self, file_name):
+        self.file_path = "./Data" + os.sep + file_name
+
+    def return_yaml_data(self):
+        with open(self.file_path, "r",encoding="utf-8") as f:
+            return yaml.load(f)
+
+
+
+
+
+
+
